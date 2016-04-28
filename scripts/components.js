730f89b04685564b2231071c819b056a84e06d3b
Sam <sambrown52@hotmail.co.uk>
2016-04-28 23:03:44 +0100

    Initial commit

---
+++ scripts/components.js
@@ -0,0 +1,26 @@
+class App extends React.Component {
+  render() {
+    return <h2>Hello From React</h2>;
+  }
+}
+
+class Paragraph extends React.Component {
+  render() {
+    return <p>Here is some information created by React</p>;
+  }
+}
+
+class Time extends React.Component {
+  render() {
+	let now = new Date();
+    return (
+	<div>
+	<p> The current time is: {now.toTimeString()} </p>
+	</div>
+)}}
+
+
+ReactDOM.render( < App/>, document.getElementById('root'));
+ReactDOM.render( < Paragraph/>, document.getElementById('para'));
+
+ReactDOM.render( < Time/>, document.getElementById('time'));
